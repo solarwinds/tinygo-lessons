1aa31a78d903ca97cce88bf1650ca798bc65973e
Trevor Rosen <trevor@catapult-creative.com>
2019-10-29 14:31:39 -0500

    Ignore bin; add lesson0

---
+++ lesson0/main.go
@@ -0,0 +1,17 @@
+package main
+
+//
+// Blinking an onboard LED is the "hello, world!" of programming small devices...
+//
+func main() {
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	for {
+		led.Low()
+		time.Sleep(time.Millisecond * 500)
+
+		led.High()
+		time.Sleep(time.Millisecond * 500)
+	}
+}
+
